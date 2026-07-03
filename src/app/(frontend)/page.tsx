9e0f3b9818e87cef8d452eb020eef5ead01df269
Amin Zare <cwpslxck@gmail.com>
2026-07-03 18:26:33 +0330

    style: better main page spaceing

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -7,16 +7,16 @@ export default async function HomePage() {
   const { docs: posts } = await getPublishedPosts()
 
   return (
-    <BlogContainer className="py-10">
-      <div className="relative z-[1] my-6 w-full">
-        <h1 className="text-3xl font-semibold leading-10 tracking-tighter text-foreground">
+    <BlogContainer className='py-6'>
+      <div className="relative z-[1] w-full">
+        <h1 className="text-3xl mb-6 font-semibold tracking-tighter text-foreground">
           {siteConfig.tagline}
         </h1>
 
         {posts.length === 0 ? (
-          <p className="mt-12 text-sm text-muted-foreground">هنوز مطلبی منتشر نشده. از پنل مدیریت یک مطلب بسازید.</p>
+          <p className="text-sm text-muted-foreground">هنوز مطلبی منتشر نشده. از پنل مدیریت یک مطلب بسازید.</p>
         ) : (
-          <div className="mt-12 grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-4">
+          <div className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-4">
             {posts.map((post) => (
               <PostListItem key={post.id} post={post} />
             ))}
